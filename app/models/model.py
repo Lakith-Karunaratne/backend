b158b4043bda32d121e3988f4f7d3aea1849716a
lakithk <Lakith.k@outlook.com>
2023-10-07 03:06:01 +0530

    added model user creation with hashing

--- app/models/model.py
+++ app/models/model.py
@@ -1,10 +1,16 @@
 import uuid
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Date, UniqueConstraint
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
-from app.db.database import Base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from passlib.context import CryptContext
+from sqlalchemy import event
+from sqlalchemy.exc import IntegrityError
 
+Base = declarative_base() # For Model Migrations
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class BaseModel(Base):
     __abstract__ = True
@@ -26,6 +32,27 @@ class User(BaseModel):
             f"User id={self.auto_id}, uuid={self.uuid}, date={self.date_created}, updated={self.date_updated}\
                 username={self.username},email={self.email}"
         )
+    
+    @classmethod
+    def create_user(cls, username, email, password):
+        try:
+            new_user = cls(username=username, email=email, pw_hash=password)
+            session.add(new_user)
+            session.commit()
+            return "User created successfully"
+        except IntegrityError as e:
+            # Handle unique constraint violation (e.g., duplicate username or email)
+            session.rollback()
+            return "User with the same username or email already exists"
+    
+    def verify_password(self, password):
+        return password_context.verify(password, self.pw_hash)
+
+@event.listens_for(User, 'before_insert')
+def hash_user_password(mapper, connection, target):
+    # Hash the user's password before insertion
+    target.pw_hash = password_context.hash(target.pw_hash)
+
 
 class Book(BaseModel):
     __tablename__ = 'books'
@@ -45,3 +72,27 @@ class Book(BaseModel):
                 author={self.author},title={self.title}"
         )
 
+if __name__ == "__main__":
+
+    print("Testing model.py")
+
+    user = [
+        User(username='admin',email='admin@books.com',pw_hash='abc123')
+        ]
+
+    engine = create_engine('postgresql+psycopg2://admin_db:abcd1234@localhost:5432/dev_db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Create a new user and insert it into the database
+    # new_user = User(username='Test User', email='test@example.com', pw_hash='abc123')
+    # session.add(new_user)
+    # session.commit()
+
+    for _ in user:
+        session.add(_)
+
+    session.commit()
+    session.close()
+
+    
