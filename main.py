9bf442958d627bcc8ac6e28b27243f1becb28d27
lakithk <Lakith.k@outlook.com>
2023-10-07 17:21:17 +0530

    main py updated

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.env import APPENV
 from app.db import database
 from app.modules.auth import auth
 from app.modules.book_core import book_api
+
 # from initial_data import add_first_user
 # from contextlib import asynccontextmanager
 
@@ -17,13 +18,17 @@ from app.modules.book_core import book_api
 app = FastAPI()
 
 app.include_router(auth.router)
-app.include_router(book_api.router)
+app.include_router(book_api.router, dependencies=[Depends(auth.login_manager)])
 
 print(APPENV.get_sql())
 # add_first_user()
 # @app.on_event("startup")
 # async def startupevet():
 #     await add_first_user()
+
+@app.get('/')
+def root():
+    return {"detail":"personal book lib api"}
 
 if __name__ == "__main__":
     PORT = 8000
